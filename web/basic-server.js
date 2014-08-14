ae07398abf8436f36a65d8b48585cbe796ca7653
nrobeR <mr.nrober@gmail.com>
2014-08-14 13:49:22 -0700

    Working on cron job

--- web/basic-server.js
+++ web/basic-server.js
@@ -2,18 +2,16 @@ var http = require("http");
 var handler = require("./request-handler");
 var helpers = require('./http-helpers.js');
 var archiveManager = require('./archiveManager');
-var archive = require('../helpers/archive-helpers');
 
 var port = 8080;
 var ip = "127.0.0.1";
-var blarg = function(){}
-var bleh = function(x, y){console.log('sytlesheet fired');return handler.handleRequest.bind(null, x,y, archive.paths.siteAssets + '/styles.css'); }
+var favicon = function(){}
 var routeMap = {
   '/': handler.handleRequest,
   'check' : archiveManager.handleCheck,
   '404': handler.throw404,
-  '/favicon.ico' : blarg,
-  '/public/styles.css': bleh
+  '/favicon.ico' : favicon,
+  '/public/styles.css': handler.handleCSS
 
 }
 
